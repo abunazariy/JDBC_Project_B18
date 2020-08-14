3149b3f0405efd49a955c940e9ba74759292c17f
Abdulla Nazarov <abunazariy@gmail.com>
2020-08-14 04:27:38 -0400

    update

---
+++ src/test/java/com/cybertek/jdbc/day01/ResultSetMetaDataPractice.java
@@ -0,0 +1,36 @@
+package com.cybertek.jdbc.day01;
+
+import java.sql.*;
+
+public class ResultSetMetaData {
+    public static void main(String[] args) throws SQLException {
+        String connectionStr = "jdbc:oracle:thin:@3.93.69.55:1521:XE";
+        String userName = "hr";
+        String password = "hr";
+
+        Connection connection = DriverManager.getConnection(connectionStr,userName,password);
+
+        Statement stmnt = connection.createStatement(java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet rs = stmnt.executeQuery("select * from countries");
+
+        java.sql.ResultSetMetaData rsmd = rs.getMetaData();
+        System.out.println("rsmd.getColumnCount() = " + rsmd.getColumnCount());
+
+
+        rs.close();
+        stmnt.close();
+        connection.close();
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
